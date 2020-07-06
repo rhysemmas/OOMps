00b202688eb8a836dcc61537ef09d43bce573c6f
Rhys Emmas <44974642+rhysemmas@users.noreply.github.com>
2020-07-06 15:39:15 +0100

    4.0 and 4.1 (#2)
    * Maintain an array of pointers to each allocated block
    
    * Update for 4.0
    
    * Allocate array of pointers to the heap

--- main.cpp
+++ main.cpp
@@ -3,20 +3,31 @@
 #include <cstring>
 
 #define CHUNK_SIZE 4096
-#define NUM_CHUNKS 29884416
-// 4096 * 29884416 = 122406567936 bytes (114GB)
+#define NUM_CHUNKS 26214400 // 100gb
 //#define NUM_CHUNKS 524288 // 2gb
+
+// CHUNK_SIZE * NUM_CHUNKS = memory usage (bytes)
+// e.g: 4096 * 29884416 = 122406567936 bytes (114GB)
+// array of pointers will use NUM_CHUNKS * 8 bytes (64 bit)
+// 2gb memory usage with 4k blocks = 4mb of memory usage for array of pointers
 
 using namespace std;
 
 int main()
 {
   int n = NUM_CHUNKS;
+  // allocate on the heap - very large arrays will not fit on the stack
+  // the line commented out below will be ok for the 2gb example
+  //void * ptr[NUM_CHUNKS];
+  void **ptr = new void*[NUM_CHUNKS];
+
   for (int i = 0; i < n; i++)
   // with i being less than n, we should be short 1 * 4k block
   {
-    void* ptr = malloc(CHUNK_SIZE);
-    if (ptr == nullptr)
+    ptr[i] = malloc(CHUNK_SIZE);
+    // malloc returns a pointer to the start of a the block
+    cout << "Start of block: " << ptr[i] << endl;
+    if (ptr[i] == nullptr)
     {
       // allocation failed, we will probably get OOMed before ever seeing this
       cout << "Oh dear" << endl;
@@ -24,7 +35,7 @@ int main()
     else
     {
       // memory allocated, set memory chunk to zero's
-      cout << "Allocated block" << endl;
+      cout << "Initialising allocated block" << endl;
       memset(ptr, 0, CHUNK_SIZE);
 
       if (i == n - 1)
